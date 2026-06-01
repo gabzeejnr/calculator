41ea2f22d4259d720af42e284cd3d4da834df442
gabzeejnr <gabrieldodowei@gmail.com>
2026-06-01 21:30:52 +0100

    Created setOperator to handle operator symbols

--- script.js
+++ script.js
@@ -1,66 +1,67 @@
 let display = document.getElementById('display-el');
 const numbers = document.querySelectorAll('.number');
 const clearButton = document.querySelector('#clear');
-      
+
 //Operators' Call
 const addButton = document.querySelector('#add');
 const subtractButton = document.querySelector('#subtract');
 const multiplyButton = document.querySelector('#multiply');
 const divideButton = document.querySelector('#divide');
 const equalButton = document.querySelector('#equal');
-      
-      
-const operatorSign = document.querySelectorAll('.operator');
-let firstNumber, secondNumber, operator, output;
-      
+
+let firstNumber, secondNumber, operator, result;
+
 //This handles the clear button
 clearButton.addEventListener('click', () => {
-        display.value = ""
-})
-        
+  display.value = "";
+});
+
 //This part of code controls the number display into the input...
 numbers.forEach( (number) => {
   number.addEventListener('click', () => {
     display.value += number.textContent;
   });
 });
-      
-/* //Operation Base😭😭😭
-operatorSign.forEach((operator) => {
-  operator.addEventListener('click', () => {
-    firstNumber = display.value;
-    display.value = "";
-    secondNumber = display.value;
-          
-    if (operator === '=') {
-            
-      if(operator === '+') {
-        output = Number(firstNumber) + Number(secondNumber);
-        console.log(output)
-      }
-      if (operator === '-') {
-        output = Number(firstNumber) - Number(secondNumber);
-      }
-      if (operator === '/') {
-        output = Number(firstNumber) / Number(secondNumber);
-      }
-      if (operator === '*') {
-        output = Number(firstNumber) * Number(secondNumber);
-      }
-      console.log(output);
-    }
-  });
-}) */
+
 
 //Operations Base... Again
 
-addButton.addEventListener('click', () => {
+function setOperator(op) {
   firstNumber = display.value;
+  operator = op;
   display.value = "";
-  
-  equalButton.addEventListener('click', () => {
-    secondNumber = display.value;
+};
+
+addButton.addEventListener('click', () => {
+  setOperator('+');
+});
+subtractButton.addEventListener('click', () => {
+  setOperator('-');
+});
+divideButton.addEventListener('click', () => {
+  setOperator('/')
+});
+multiplyButton.addEventListener('click', () => {
+  setOperator('*')
+});
+
+equalButton.addEventListener('click', () => {
+  secondNumber = display.value;
+  if (operator === '+') {
     result = Number(firstNumber) + Number(secondNumber);
     display.value = result;
-  });
-})
+  };
+  if (operator === '-') {
+    result = Number(firstNumber) - Number(secondNumber);
+    display.value = result;
+  };
+  if (operator === '/') {
+    result = Number(firstNumber) / Number(secondNumber);
+    display.value = result;
+  };
+  if (operator === '*') {
+    result = Number(firstNumber) * Number(secondNumber);
+    display.value = result;
+  };
+  firstNumber = result;
+});
